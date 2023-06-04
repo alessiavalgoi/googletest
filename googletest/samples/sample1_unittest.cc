26298dda32b05a951b7ddeee7c0f4df09b483df7
alessiavalgoi <119410022+alessiavalgoi@users.noreply.github.com>
2023-06-04 16:15:33 +0200

    Update sample1_unittest.cc

--- googletest/samples/sample1_unittest.cc
+++ googletest/samples/sample1_unittest.cc
@@ -141,6 +141,8 @@ int main(int argc, char** argv) {
   return RUN_ALL_TESTS();
 }
 
+//cc
+
 // Step 3. Call RUN_ALL_TESTS() in main().
 //
 // We do this by linking in src/gtest_main.cc file, which consists of
